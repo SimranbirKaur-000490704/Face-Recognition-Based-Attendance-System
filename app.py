bf9ed85e6058154e4427018c60eb5b60af530614
Simranbir Kaur <simranbirkaur@Simranbirs-MBP.gateway.sasktel.net>
2024-02-20 14:41:32 -0600

    registration page functionality changes

--- app.py
+++ app.py
@@ -80,49 +80,6 @@ def generate_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + face + b'\r\n')"""
 
-# Load your webcam capture script here
-
-
-"""def generate_frames():
-    # Load the pre-trained Haar Cascade classifier for face detection
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-    cap = cv2.VideoCapture(0)
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            # Detect faces in the frame
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-            # Encode frame to JPEG format
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            
-            if len(faces) > 0:
-                # If faces are detected, capture and yield only the first detected face
-                x, y, w, h = faces[0]
-                
-                # Decode the frame bytes to a NumPy array
-                frame_array = np.frombuffer(frame, dtype=np.uint8)
-                frame_array = cv2.imdecode(frame_array, cv2.IMREAD_COLOR)
-                
-                # Extract the face region from the frame array
-                face = frame_array[y:y+h, x:x+w]
-                
-                # Encode the face region to JPEG format and yield it
-                ret, buffer = cv2.imencode('.jpg', face)
-                face_bytes = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + face_bytes + b'\r\n')
-            else:
-                # If no faces are detected, yield the entire frame
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')"""
-
-
 
 # Open the webcam window
 """def open_webcam1():
@@ -153,16 +110,25 @@ def generate_frames():
 # Saving the image in folder
 @app.route('/save_image', methods=['POST'])
 def handle_save_image():
-    print("saveimgae method")
-    image_data = request.json['image_data']
+    print("saveimage method", request.json.get("jsonData") )
 
-    #Calling Save_image function
-    save_image(image_data)
+    json_data = request.json  # Accessing jsonData from the request JSON payload
+    if json_data:
+        image_data = json_data.get("image_data")  # Accessing image_data from jsonData
+        form_data = json_data.get("form_data") 
 
-    #save_image(image_data)
-    return 'Image saved successfully!', 200
+        #Calling Save_image function , send image data and form data to fetch name and id to label the image data
+        save_image(image_data, form_data)
 
-def save_image(image_data):
+        #Calling save_form_data to save the form details
+        save_form_data(form_data)
+
+        return 'Image saved successfully!', 200
+
+    else:
+        return "Invalid request", 400
+
+def save_image(image_data, form_data):
     print("in save images method")
     image_data = image_data.split(',')[1]  # Remove 'data:image/jpeg;base64,' prefix
     nparr = np.frombuffer(base64.b64decode(image_data), np.uint8)
@@ -173,11 +139,36 @@ def save_image(image_data):
     cropped_image = image_cleaning_resizing(img)
 
     #Calling generate unique id function
-    image_id = generate_unique_id()
+    #image_id = generate_unique_id()
+
+    #Creating label for the image
+    id = form_data.get("Student Id") 
+    name = form_data.get("Student Name")
+    image_id =  id+"_"+name
+
+    #Creating a directory with person's name
+    directory = os.path.join('images', name)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
     # Save the cropped image to a folder with the unique ID as the filename
-    image_path = os.path.join('images', f'{image_id}.jpg')
+    # if the same person comes in and create multiple pics , it will save the images with a numeric labels appended with it.
+    image_filename = f'{image_id}.jpg'
+    count = 1
+    while os.path.exists(os.path.join(directory, image_filename)):
+        image_filename = f'{image_id}_{count}.jpg'
+        count += 1
+            
+    image_path = os.path.join(directory, image_filename)
     cv2.imwrite(image_path, cropped_image)
+
+    #image_path = os.path.join(directory, f'{image_id}.jpg')
+    #cv2.imwrite(image_path, cropped_image)
+
+    # Save the cropped image to a folder with the unique ID as the filename
+   # image_path = os.path.join('images', f'{image_id}.jpg')
+    #cv2.imwrite(image_path, cropped_image)
 
 
 #Function to detect face and crop the image, resize it and return
@@ -217,70 +208,44 @@ def image_cleaning_resizing(image):
 
         return face_resized
 
-    """if len(faces) > 0:
-        # Get the coordinates of the first detected face
-        x, y, w, h = faces[0]
-        
-        # Crop the region containing the face
-        cropped_image = image[y:y+h, x:x+w]
-        
-        return cropped_image
-    else:
-        # If no faces are detected, return None
-        return None"""
-    
-"""def image_cleaning_resizing(image):
-    # Load the pre-trained Haar Cascade classifier for face detection
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+#Saving the form data 
+def save_form_data(form_data):
+    csv_file_path = 'form_data.csv'
+    record_exists = False
 
-   # cap = cv2.VideoCapture(0)
-    #while True:
-        #success, frame = cap.read()
-       # if not success:
-        #    break
-    if(image):
-        # Detect faces in the frame
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    # Read existing records from CSV file
+    existing_records = []
+    with open(csv_file_path, 'r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            existing_records.append(row)
 
-        # Encode frame to JPEG format
-        ret, buffer = cv2.imencode('.jpg', image)
-        image = buffer.tobytes()
-            
-        if len(faces) > 0:
-             # If faces are detected, capture and yield only the first detected face
-            x, y, w, h = faces[0]
-                
-            # Decode the frame bytes to a NumPy array
-            frame_array = np.frombuffer(image, dtype=np.uint8)
-            frame_array = cv2.imdecode(frame_array, cv2.IMREAD_COLOR)
-                
-            # Extract the face region from the frame array
-            face = frame_array[y:y+h, x:x+w]
-                
-            # Encode the face region to JPEG format and yield it
-            ret, buffer = cv2.imencode('.jpg', face)
-            face_bytes = buffer.tobytes()
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + face_bytes + b'\r\n')
-        else:
-            # If no faces are detected, yield the entire frame
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')"""
+    # Check if the record already exists
+    for record in existing_records:
+        if record['Student Id'] == form_data['Student Id']:
+            record_exists = True
+            record.update(form_data)
+            break
 
-#Saving the form data in csv
-@app.route('/save_form_data', methods=['POST'])
-def save_form_data():
-    data = request.json  # Get form data
-    with open('form_data.csv', 'a', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=data.keys())
-        if csvfile.tell() == 0:  # Write header if file is empty
-            writer.writeheader()
-        writer.writerow(data)  # Write form data to CSV
+    # Write records back to CSV file
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        fieldnames = form_data.keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for record in existing_records:
+            if record['Student Id'] != form_data['Student Id']:
+                writer.writerow(record)
+
+        # Write the updated record if it exists
+        if record_exists:
+            writer.writerow(form_data)
+
     return 'Form data saved successfully!', 200
 
     
-# Function to generate a unique ID for each image
+# This can be deleted , Function to generate a unique ID for each image
 def generate_unique_id():
     return str(uuid.uuid4())
 
